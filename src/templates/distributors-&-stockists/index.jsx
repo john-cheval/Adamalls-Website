77894f62f557eeedb437434c6f7f86e249f19103
john-cheval <johncheval1@gmail.com>
2025-01-15 10:48:06 +0400

    first modification has been done

--- src/templates/distributors-&-stockists/index.jsx
+++ src/templates/distributors-&-stockists/index.jsx
@@ -73,59 +73,61 @@ const DistributorsAndStockistsTemplate = ({ Cards, Banner }) => {
 
 
       <div className="mt-10 md:my-[68px] flex flex-wrap gap-[24px]">
-        {
-          data?.map((distributor, index) =>
-            <div key={distributor?.title} className=" basis-full flex-1 lg:basis-[40%] relative border flex flex-col lg:flex-row min-h-[360px]">
-              <div className="flex-1 pl-[64px] pr-[57px] border-r border-b min-h-[220px] flex justify-center items-center md:!items-start">
-                <div className="z-[10] relative lg:mt-[100px] h-[155px] flex items-center justify-center">
-                  <Image
-                    width={198}
-                    height={198}
-                    alt='Distributors-logo'
-                    src={distributor?.image}
-                    className='z-[10]'
-                  />
-                </div>
-              </div>
-              <div className="flex-1 pt-[42px] pl-[64px] pr-[57px]">
-                <p className="text-theme-main font-bold text-lg text-center md:text-left md:text-[30px] leading-[44px] mb-3">{distributor?.title}</p>
-                <div className="mb-[150px] flex flex-col justify-center md:justify-start md:items-start">
-                  <p
-                    ref={(el) => descriptionRefs.current[index] = el}
-                    className={`text-[#3E3E3E] font_calibri text-center md:text-left text-xs inline-block font-light md:text-lg leading-[26px] ${expanded[distributor.title] ? '' : 'truncate'}`}
-                    style={{
-                      whiteSpace: 'wrap',
-                      maxHeight: expanded[distributor.title] ? 'none' : '140px',
-                      overflow: 'hidden',
-                      display: '-webkit-box',
-                      WebkitLineClamp: expanded[distributor.title] ? 'none' : 5,
-                      WebkitBoxOrient: 'vertical',
-                    }}
-                  >
-                    {distributor.description}
-                  </p>
-                  {showButton[distributor.title] &&
-                    <button
-                      className="inline-block text-theme-main text-sm md:text-base"
-                      onClick={() => toggleExpand(distributor.title)}
-                    >
-                      {expanded[distributor.title] ? 'Read Less' : 'Read More'}
-                    </button>
-                  }
-                </div>
-              </div>
-              <div style={{
-                transform: 'translateX(-50%)'
-              }} className="absolute z-[0] left-[50%] bottom-[42px] bg-white pt-[41px] pb-[38px]  ">
-                <a className='whitespace-nowrap bg-white text-sm md:text-lg font-bold leading-[26px] text-theme-main py-4 px-[65px] border border-[#B2B6E0] rounded-[30px]' href={distributor?.link} target='_blank'>Visit Website</a>
-              </div>
+  {
+    data?.map((distributor, index) =>
+      <div key={distributor?.title} className="basis-full flex-1 lg:basis-[40%] relative border border-[#B2B6E0] flex flex-col min-h-[360px]">
+        <div className="flex flex-row flex-grow">
+          <div className="flex-1 pl-[64px] pr-[57px] border-r border-r-[#B2B6E0] min-h-[220px] flex justify-center items-center md:!items-start">
+            <div className="z-[10] relative lg:mt-[100px] h-[155px] flex items-center justify-center">
+              <Image
+                width={198}
+                height={198}
+                alt='Distributors-logo'
+                src={distributor?.image}
+                className='z-[10]'
+              />
             </div>
-          )
-        }
-        {data.length % 2 !== 0 && <div className='basis-full flex-1 hidden md:flex justify-center flex-col items-center bg-gradient-to-r from-[#2E368F] to-[#B2B6E0] opacity-75 lg:basis-[40%] relative border min-h-[360px]'>
-          <Image src="/svg/site-fav.svg" alt="" width={174} height={84} />
-        </div>}
+          </div>
+          <div className="flex-1 pt-[42px] pl-[64px] pr-[57px] flex flex-col">
+            <p className="text-theme-main font-bold text-lg text-center md:text-left md:text-[30px] leading-[44px] mb-3">{distributor?.title}</p>
+            <div className="flex-grow mb-[150px] flex flex-col justify-center md:justify-start md:items-start">
+              <p
+                ref={(el) => descriptionRefs.current[index] = el}
+                className={`text-[#3E3E3E] font_calibri text-center md:text-left text-xs inline-block font-light md:text-lg leading-[26px] ${expanded[distributor.title] ? '' : 'truncate'}`}
+                style={{
+                  whiteSpace: 'wrap',
+                  maxHeight: expanded[distributor.title] ? 'none' : '140px',
+                  overflow: 'hidden',
+                  display: '-webkit-box',
+                  WebkitLineClamp: expanded[distributor.title] ? 'none' : 5,
+                  WebkitBoxOrient: 'vertical',
+                }}
+              >
+                {distributor.description}
+              </p>
+              {showButton[distributor.title] &&
+                <button
+                  className="inline-block text-theme-main text-sm md:text-base"
+                  onClick={() => toggleExpand(distributor.title)}
+                >
+                  {expanded[distributor.title] ? 'Read Less' : 'Read More'}
+                </button>
+              }
+            </div>
+          </div>
+        </div>
+        <div className="border-t border-t-[#B2B6E0]"></div>
+        <div className="flex justify-center gap-8 py-4  items-center mt-4 mb-4">
+          <a className='whitespace-nowrap bg-white text-sm md:text-lg font-bold leading-[26px] text-theme-main py-4 px-[65px] border border-[#B2B6E0] rounded-[30px]' href={distributor?.link} target='_blank'>Visit Website</a>
+          <a className='whitespace-nowrap bg-white text-sm md:text-lg font-bold leading-[26px] text-theme-main py-4 px-[65px] border border-[#B2B6E0] rounded-[30px]' href="mailto:adamallys@adamallys-llc.com" target='_self'>Enquire Now</a>
+        </div>
       </div>
+    )
+  }
+  {data.length % 2 !== 0 && <div className='basis-full flex-1 hidden md:flex justify-center flex-col items-center bg-gradient-to-r from-[#2E368F] to-[#B2B6E0] opacity-75 lg:basis-[40%] relative border min-h-[360px]'>
+    <Image src="/svg/site-fav.svg" alt="" width={174} height={84} />
+  </div>}
+</div>
     </main>
   )
 }
